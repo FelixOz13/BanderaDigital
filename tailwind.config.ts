bca85437951ea240d36a14d827ca17fbf6faac24
Felix Ozuna <ozunafelix019@gmail.com>
2024-02-10 01:32:08 -0600

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-     "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./src/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Include MDX files if you're using them
   ],
- 
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
-   
   theme: {
     extend: {
       fontFamily: {
-        primary:["Poppins"],
+        primary: ["Poppins"],
       },
     },
   },
   plugins: [],
-}
-
+};
