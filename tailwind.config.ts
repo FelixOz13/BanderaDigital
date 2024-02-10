ec889be8d816e1f1ea8a297d41e39b4da7547f4e
Felix Ozuna <ozunafelix019@gmail.com>
2024-02-10 00:27:04 -0600

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+   purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+  },
   theme: {
     extend: {
       fontFamily: {
