9d2fe7835e8935bc69edaeb9574775d3eb5776e0
Felix Ozuna <ozunafelix019@gmail.com>
2024-02-12 19:49:43 -0600

    commit

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -10,22 +10,23 @@ const Services = () => {
         <div data-aos="fade-right">
         <div className="bg-orange-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
           <CodeBracketSquareIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
-          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Frontend</h1>
-          <p className="text-[15px] text-[#d3d2d2] font-normal">HTML, CSS y JavaScript para el desarrollo frontend, con experiencia en estilizado utilizando Bootstrap, Material UI y Tailwind CSS. Experimentado en TypeScript y Next.js para la construcción de aplicaciones web robustas y eficientes.</p>
+          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Diseño Web</h1>
+          <p className="text-[15px] text-[#d3d2d2] font-normal">
+Transformamos ideas en experiencias digitales impactantes. Nuestro equipo fusiona diseño innovador, tecnología avanzada y estrategias de usuario para crear sitios web excepcionales.</p>
           </div>
         </div>
         <div data-aos="zoom-in" data-aos-delay="300">
          <div className="bg-green-700 hover:scale-110 transform transition-all duration-300 uppercase font-semibold text-center p-[2rem]">
           <RocketLaunchIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
-          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Backend</h1>
-          <p className="text-[15px] text-[#d3d2d2] font-normal">Especializados en Node.js, Express.js y MongoDB para una arquitectura backend robusta. Experimentado en GraphQL para un desarrollo eficiente de API, junto con SQL para la gestión de bases de datos relacionales, y Docker para soluciones de implementación escalables.</p>
+          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Desarollo Movil</h1>
+          <p className="text-[15px] text-[#d3d2d2] font-normal">Potenciamos tu presencia en dispositivos móviles con aplicaciones personalizadas. Desde el diseño hasta la implementación, creamos soluciones móviles intuitivas y atractivas para tu negocio.</p>
           </div>
         </div>
         <div data-aos="fade-left" data-aos-delay="500">
          <div className="bg-blue-700 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]">
           <DevicePhoneMobileIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]" />
-          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Desarollo Movil</h1>
-          <p className="text-[15px] text-[#d3d2d2] font-normal">Proficientes en el desarrollo de aplicaciones móviles multiplataforma utilizando Flutter y React Native. Hábil en la creación de experiencias nativas para las plataformas iOS y Android, con experiencia en diseño de UI/UX, gestión de estado e integración con servicios backend para aplicaciones móviles fluidas.</p>
+          <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Redes Sociales </h1>
+          <p className="text-[15px] text-[#d3d2d2] font-normal">Maximiza tu visibilidad y compromiso en línea con nuestra experiencia en publicidad y gestión de redes sociales. Desde estrategias de contenido hasta campañas publicitarias, impulsamos tu presencia digital.</p>
         </div>
         </div> 
         </div>
--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -5,7 +5,7 @@ import SkillsLanguage from "@/Components/SkillsLanguage";
 const Skills = () => {
   return (
     <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
-      <h1 className="heading">Herra<span className="text-orange-400">mientas</span></h1>
+      <h1 className="heading">Especia<span className="text-orange-400">listas en</span></h1>
       <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] items-center">
         <div>
           <div>
@@ -13,15 +13,15 @@ const Skills = () => {
           </div>
           <div className="md:col-span-2"> {/* This div spans two columns on medium screens and above */}
             <SkillsLanguage 
-              skill1="HTML"
-              skill2="CSS"
-              skill3="Javascript"
-              skill4="Node"
-              skill5="Typescript"
-              skill6="Desarollo Movil"
-              level1="w-[91%]"
-              level2="w-[88%]"
-              level3="w-[80%]"
+              skill1="Desarrollo Movil"
+              skill2="Marketing Digital"
+              skill3="Redes Sociales"
+              skill4="Creacion de Sitios"
+              skill5="Diseño Web"
+              skill6="Programacion Analitica"
+              level1="w-[100%]"
+              level2="w-[100%]"
+              level3="w-[100%]"
             />
           </div>
         </div>
